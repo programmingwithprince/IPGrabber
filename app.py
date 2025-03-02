ec8937e440fd6fb934527def4c0c90de8ef3edc1
ProgrammingWithPrince/ <86817604+programmingwithprince@users.noreply.github.com>
2025-03-02 16:19:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,6 @@ def get_image():
     user_agent = request.headers.get('User-Agent')  # Get browser info
     referrer = request.referrer  # Get referrer (if available)
 
-    # Fetch extra details from ipinfo.io
-    ip_details = get_ip_info(user_ip)
-
     # Log info to console
     print(f"IP: {user_ip}, User-Agent: {user_agent}, Referrer: {referrer}")
 
